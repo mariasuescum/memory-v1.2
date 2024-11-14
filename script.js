2df93f36a7bdf03e74f7085183cdbcdeace8792b
maria suescum <andreinasuescum@gmail.com>
2024-11-14 17:53:46 -0500

    popup-mob

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ function showPopup(image) {
 
     setTimeout(() => {
         popup.style.display = "none";  // Oculta el pop-up después de 6 segundos
-    }, 10000);
+    }, 9000);
 }
 
 // Inicia el juego mostrando el tablero
